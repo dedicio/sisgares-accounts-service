0cc6c9c9d82d5354ed83a7ea0d5c486768b1508d
Dedicio Coelho <dedicio@gmail.com>
2023-09-28 13:42:53 -0700

    feat: create responses

---
+++ pkg/response/user.go
@@ -0,0 +1,31 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/dedicio/sisgares-accounts-service/internal/dto"
+)
+
+type UserResponse struct {
+	*dto.UserResponseDto
+}
+
+func (pr *UserResponse) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func NewUserResponse(product *dto.UserResponseDto) *UserResponse {
+	return &UserResponse{product}
+}
+
+type UsersResponse struct {
+	Users []*dto.UserResponseDto
+}
+
+func (pr *UsersResponse) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func NewUsersResponse(products []*dto.UserResponseDto) *UsersResponse {
+	return &UsersResponse{products}
+}
